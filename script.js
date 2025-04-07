ba58609b6a58b3490f27b6feb522bbb1abdc067e
ronniesilvas <38744731+ronniesilvas@users.noreply.github.com>
2025-04-07 16:59:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  databaseURL: "TU_DATABASE_URL",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBLGzaeqqam1MhdaNBa4jL8X3pFgAxq7BE",
+  authDomain: "control-mantto.firebaseapp.com",
+  databaseURL: "https://control-mantto-default-rtdb.firebaseio.com",
+  projectId: "control-mantto",
+  storageBucket: "control-mantto.firebasestorage.app",
+  messagingSenderId: "634547171528",
+  appId: "1:634547171528:web:8a20ee90fe01819178b5fd",
+  measurementId: "G-ER6CBY2J3Y"
 };
 
 firebase.initializeApp(firebaseConfig);
